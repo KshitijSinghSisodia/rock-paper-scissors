245d31b5b64fe2fa2c0540e8d06f6d635eea291b
Kshitij <79467163+3sterisk@users.noreply.github.com>
2023-05-09 20:35:55 +0530

    Added the game function for five round match: declares the winner of best of 5!

--- script.js
+++ script.js
@@ -18,14 +18,37 @@ function round(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
   if (playerSelection == computerSelection) {
-    return "It's a Draw!";
+    console.log("It's a Draw!");
+    return [0, 0];
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return [1, 0];
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return [1, 0];
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return [1, 0];
   } else {
-    return `You Loose! ${computerSelection} beats ${playerSelection}`;
+    console.log(`You Loose! ${computerSelection} beats ${playerSelection}`);
   }
+  return [0, 1];
 }
+
+function game() {
+  let computer = 0;
+  let player = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Enter your choice [Rock, Paper or Scissors]");
+    let res = round(playerSelection, getComputerChoice());
+    computer += res[1];
+    player += res[0];
+  }
+  if (player === computer) console.log("Draw");
+  else if (player > computer) console.log("You win!! <3");
+  else console.log("You lost! Better luck next time!!");
+  player = 0;
+  computer = 0;
+}
+
+game();
